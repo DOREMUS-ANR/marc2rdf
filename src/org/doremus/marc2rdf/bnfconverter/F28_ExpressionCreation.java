51768ba8ed46ec8f286d8a9dd211bc207b300367
fernanev <evafm37@gmail.com>
2016-04-12 11:18:43 +0200

    Update "Date en format machine" Close #3

--- src/org/doremus/marc2rdf/bnfconverter/F28_ExpressionCreation.java
+++ src/org/doremus/marc2rdf/bnfconverter/F28_ExpressionCreation.java
@@ -76,24 +76,111 @@ public class F28_ExpressionCreation {
         	 Record s = reader.next();
         	 for (int i=0; i<s.controlFields.size(); i++) {
         		 
-        		 /***************************** Cas 1 *********************************/
         		 if (s.controlFields.get(i).getEtiq().equals("008")) {
+            		 /***************************** Cas 1 *********************************/
         			 if (((s.controlFields.get(i).getData().charAt(36))==((" ").charAt(0)))&&
         			 ((s.controlFields.get(i).getData().charAt(46))==((" ").charAt(0)))){
+        				String sy = ""; 
+        			 	boolean noSy = true;
         			 	for (int j=28; j<=31; j++){
-        			 		buffer.append(s.controlFields.get(i).getData().charAt(j));
+
+        			 		char character = s.controlFields.get(i).getData().charAt(j);
+        			 		sy = sy + character;
+        			 		if (!(character==(' '))) {noSy = false;}
         			 	}
-        			 	String st = "";
-        			 	boolean nothing = true;
+        			 	String sm = "";
+        			 	boolean noSm = true;
+        			 	for (int j=32; j<=33; j++){
+        			 		char character = s.controlFields.get(i).getData().charAt(j);
+        			 		sm = sm + character;
+        			 		if (!(character==(' '))) {noSm = false;}
+        			 	}
+        			 	String sd = "";
+        			 	boolean noSd = true;
+        			 	for (int j=34; j<=35; j++){
+        			 		char character = s.controlFields.get(i).getData().charAt(j);
+        			 		sd = sd + character;
+        			 		if (!(character==(' '))) {noSd = false;}
+        			 	}
+        			 	String fy = ""; 
+        			 	boolean noFy = true;
         			 	for (int j=38; j<=41; j++){
         			 		char character = s.controlFields.get(i).getData().charAt(j);
-        			 		st = st+ character;
-        			 		if (!(character==(' '))) {nothing = false;}
+        			 		fy = fy + character;
+        			 		if (!(character==(' '))) {noFy = false;}
         			 	}
-        			 	if (nothing==false) {
-        			 		buffer.append("/");
-        			 		buffer.append(st);
+        			 	String fm = "";
+        			 	boolean noFm = true;
+        			 	for (int j=42; j<=43; j++){
+        			 		char character = s.controlFields.get(i).getData().charAt(j);
+        			 		fm = fm + character;
+        			 		if (!(character==(' '))) {noFm = false;}
         			 	}
+        			 	String fd = "";
+        			 	boolean noFd = true;
+        			 	for (int j=44; j<=45; j++){
+        			 		char character = s.controlFields.get(i).getData().charAt(j);
+        			 		fd = fd + character;
+        			 		if (!(character==(' '))) {noFd = false;}
+        			 	}
+       				    /******************** Sous cas 1.1 ********************************/
+        			 	if(!noSy && noSm && noSd && noFy && noFm && noFd){
+        			 		sm = "01";
+        			 		sd = "01";
+        			 		fm = "12";
+        			 		fd = "31";
+        			 	}
+       				    /******************** Sous cas 1.2 ********************************/
+        			 	else if(!noSy && !noSm && noSd && noFy && noFm && noFd){
+        			 		sd = "01";
+        			 		fy = sy;
+        			 		fm = sm;
+        			 		fd = getLastDay(fm, fy);
+        			 	}
+       				    /******************** Sous cas 1.3 ********************************/
+        			 	else if(!noSy && !noSm && !noSd && noFy && noFm && noFd){
+        			 		fy = sy;
+        			 		fm = sm;
+        			 		fd = sd;
+        			 	}
+        			 	/******************** Sous cas 1.4 ********************************/
+        			 	else if(!noSy && !noFy){
+        			 		// Begin period
+        			 		if(noSm){
+        			 			sm = "01";
+        			 			sd = "01";
+        			 		}
+        			 		else if(noSd){
+        			 			sd = "01";
+        			 		}
+        			 		// Finish period
+        			 		if(noFm && fy.equals(sy) && !noSm){
+        			 			fm = sm;
+        			 			fd = getLastDay(fm, fy);
+        			 		}
+        			 		else if(noFm){
+        			 			fm = "12";
+        			 			fd = "31";
+        			 		}
+        			 		else if(noFd){
+        			 			fd = getLastDay(fm, fy);
+        			 		}
+        			 		
+        			 	}
+       				    /******************** Sous cas 1.5 ********************************/
+        			 	if(noSy && !noFy){
+        			 		sy = "" + fy.charAt(0) + fy.charAt(1) + "00";
+        			 		sm = "01";
+        			 		sd = "01";
+        			 		if(noFm){
+        			 			fm = "12";
+        			 			fd = "31";
+        			 		}
+        			 		else if(noFd){
+        			 			fd = getLastDay(fm, fy);
+        			 		}
+        			 	}
+        			 	buffer.append(sy + sm + sd + "/" + fy + fm + fd);
         			 }
         			 /***************************** Cas 2 *********************************/
         			 else if (((s.controlFields.get(i).getData().charAt(36))==(("?").charAt(0)))||
@@ -103,44 +190,44 @@ public class F28_ExpressionCreation {
                 			 ((s.controlFields.get(i).getData().charAt(40))==((".").charAt(0)))||
                 			 ((s.controlFields.get(i).getData().charAt(41))==((".").charAt(0)))){
         				 /******************** Sous cas 2.1 ********************************/
-        				 if (((s.controlFields.get(i).getData().charAt(30))==((".").charAt(0)))&&
-        						 ((s.controlFields.get(i).getData().charAt(31))==((".").charAt(0)))){
-        				 String dt ="";
-        				 for (int j=28; j<=29; j++){
-         			 		buffer.append(s.controlFields.get(i).getData().charAt(j));
-         			 		dt = dt+s.controlFields.get(i).getData().charAt(j);
-         			 	}
-        				 buffer.append("00/"+dt+"99");
+	        				 if (((s.controlFields.get(i).getData().charAt(30))==((".").charAt(0)))&&
+	        						 ((s.controlFields.get(i).getData().charAt(31))==((".").charAt(0)))){
+	        				 String dt ="";
+	        				 for (int j=28; j<=29; j++){
+	         			 		buffer.append(s.controlFields.get(i).getData().charAt(j));
+	         			 		dt = dt+s.controlFields.get(i).getData().charAt(j);
+	         			 	 }
+        				     buffer.append("00/"+dt+"99");
         				 }
         				 /******************** Sous cas 2.2 ********************************/
         				 if (!((s.controlFields.get(i).getData().charAt(30))==((".").charAt(0)))&&
         						 ((s.controlFields.get(i).getData().charAt(31))==((".").charAt(0)))){
-        				 String dt ="";
-        				 for (int j=28; j<=30; j++){
-         			 		buffer.append(s.controlFields.get(i).getData().charAt(j));
-         			 		dt = dt+s.controlFields.get(i).getData().charAt(j);
-         			 	}
-        				 buffer.append("0/"+dt+"9");
+	        				 String dt ="";
+	        				 for (int j=28; j<=30; j++){
+	         			 		buffer.append(s.controlFields.get(i).getData().charAt(j));
+	         			 		dt = dt+s.controlFields.get(i).getData().charAt(j);
+	         			 	 }
+	        				 buffer.append("0/"+dt+"9");
         				 }
         				 /******************** Sous cas 2.3 ********************************/
         				 if (((s.controlFields.get(i).getData().charAt(40))==((".").charAt(0)))&&
         						 ((s.controlFields.get(i).getData().charAt(41))==((".").charAt(0)))){
-        				 String dt ="";
-        				 for (int j=38; j<=39; j++){
-         			 		buffer.append(s.controlFields.get(i).getData().charAt(j));
-         			 		dt = dt+s.controlFields.get(i).getData().charAt(j);
-         			 	}
-        				 buffer.append("00/"+dt+"99");
+	        				 String dt ="";
+	        				 for (int j=38; j<=39; j++){
+	         			 		buffer.append(s.controlFields.get(i).getData().charAt(j));
+	         			 		dt = dt+s.controlFields.get(i).getData().charAt(j);
+	         			 	 }
+	        				 buffer.append("00/"+dt+"99");
         				 }
         				 /******************** Sous cas 2.4 ********************************/
         				 if (!((s.controlFields.get(i).getData().charAt(40))==((".").charAt(0)))&&
         						 ((s.controlFields.get(i).getData().charAt(41))==((".").charAt(0)))){
-        				 String dt ="";
-        				 for (int j=38; j<=40; j++){
-         			 		buffer.append(s.controlFields.get(i).getData().charAt(j));
-         			 		dt = dt+s.controlFields.get(i).getData().charAt(j);
-         			 	}
-        				 buffer.append("0/"+dt+"9");
+	        				 String dt ="";
+	        				 for (int j=38; j<=40; j++){
+	         			 		buffer.append(s.controlFields.get(i).getData().charAt(j));
+	         			 		dt = dt+s.controlFields.get(i).getData().charAt(j);
+	         			 	 }
+	        				 buffer.append("0/"+dt+"9");
         				 }
         			 }
         			 /******************************************************************************/
@@ -207,5 +294,21 @@ public class F28_ExpressionCreation {
         buffer.append(aSubField); buffer.append(","); buffer.append(mSubField); buffer.append("("); buffer.append(dSubField); buffer.append(")");
         buffer.append(eSubField); buffer.append(hSubField); buffer.append(uSubField);
         return buffer.toString();
+    }
+    
+    public static String getLastDay(String fm, String fy) {
+    	String fd;
+    	if(fm.equals("04")||fm.equals("06")||fm.equals("09")||fm.equals("11")){
+ 			fd = "30";
+ 		}else if(fm.equals("02")){
+ 			if((Integer.parseInt(fy) % 4 == 0) && ((Integer.parseInt(fy) % 100 != 0) || (Integer.parseInt(fy) % 400 == 0))){
+ 				fd = "29";
+ 			}else{
+ 				fd = "28";
+ 			}
+ 		}else{
+ 			fd = "31" ;
+ 		}
+    	return fd;
     }
 }
