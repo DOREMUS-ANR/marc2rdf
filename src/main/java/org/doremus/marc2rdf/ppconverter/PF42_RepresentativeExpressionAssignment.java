d6a2f1bb7bdd65ee44ea0b7c3edf909f28b999d5
Pasquale Lisena <pasquale.lisena@eurecom.fr>
2017-01-11 17:40:00 +0100

    new uri pp - close #29

--- src/main/java/org/doremus/marc2rdf/ppconverter/PF42_RepresentativeExpressionAssignment.java
+++ src/main/java/org/doremus/marc2rdf/ppconverter/PF42_RepresentativeExpressionAssignment.java
@@ -8,13 +8,14 @@ import org.doremus.ontology.FRBROO;
 import java.net.URISyntaxException;
 
 public class PF42_RepresentativeExpressionAssignment  extends DoremusResource{
+  private static final String philarmonie = "http://data.doremus.org/organization/Philharmonie_de_Paris";
 
   public PF42_RepresentativeExpressionAssignment(Record record, String identifier) throws URISyntaxException {
     super(record, identifier);
     this.resource.addProperty(RDF.type, FRBROO.F42_Representative_Expression_Assignment);
 
     /**************************** Responsable de l'attribution **********************************/
-    this.resource.addProperty(FRBROO.R44_carried_out_by, this.model.createResource("http://data.doremus.org/Philharmonie_de_Paris"));
+    this.resource.addProperty(FRBROO.R44_carried_out_by, this.model.createResource(philarmonie));
   }
 
   public PF42_RepresentativeExpressionAssignment add(PF22_SelfContainedExpression f22) {
