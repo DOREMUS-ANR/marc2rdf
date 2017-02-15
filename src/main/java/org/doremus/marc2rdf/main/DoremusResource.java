d6914bf75b1bab9de28f26d08037f65a8bf1c1dd
Pasquale Lisena <pasquale.lisena@eurecom.fr>
2017-02-15 11:53:08 +0100

    fix

--- src/main/java/org/doremus/marc2rdf/main/DoremusResource.java
+++ src/main/java/org/doremus/marc2rdf/main/DoremusResource.java
@@ -3,6 +3,7 @@ package org.doremus.marc2rdf.main;
 
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.vocabulary.DCTerms;
 import org.doremus.marc2rdf.bnfconverter.BNF2RDF;
@@ -22,12 +23,11 @@ public abstract class DoremusResource {
   protected Resource resource;
   protected Record record;
   protected String identifier;
+  private Resource publisher;
 
   public DoremusResource(String identifier) throws URISyntaxException {
     this.identifier = identifier;
     this.model = ModelFactory.createDefaultModel();
-
-    Resource publisher;
 
     /* generate URI */
     this.className = this.getClass().getSimpleName();
@@ -35,15 +35,14 @@ public abstract class DoremusResource {
     if (this.className.startsWith("P")) {
       this.sourceDb = "pp";
       this.className = this.className.substring(1);
-      publisher = model.createResource(PP2RDF.organizationURI);
+      this.publisher = model.createResource(PP2RDF.organizationURI);
     } else
-      publisher = model.createResource(BNF2RDF.organizationURI);
+      this.publisher = model.createResource(BNF2RDF.organizationURI);
 
     this.uri = ConstructURI.build(this.sourceDb, this.className, this.identifier);
 
     /* create RDF resource */
     this.resource = model.createResource(this.uri.toString());
-    this.resource.addProperty(DCTerms.publisher, publisher);
   }
 
   public DoremusResource(Record record) throws URISyntaxException {
@@ -57,6 +56,7 @@ public abstract class DoremusResource {
   }
 
   public Resource asResource() {
+    this.resource.addProperty(DCTerms.publisher, this.publisher);
     return this.resource;
   }
 
